4017842bd4fa700b418e625384273537d55cd324
masacoco <masaonakama0329@gmail.com>
2021-11-28 15:36:19 +0900

    [add]first commit

---
+++ Resnet50/Resnet50App.swift
@@ -0,0 +1,17 @@
+//
+//  Resnet50App.swift
+//  Resnet50
+//
+//  Created by Masao Nakama on 11/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Resnet50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
